a039ba1a5bf6499c37a138e8f95a4b3e2ffc1cb4
Anartz Mugika Ledo <mugan86@gmail.com>
2020-09-10 09:22:32 +0200

    Demo de ejemplo para trabajar con Apollo Client 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { Apollo, gql } from 'apollo-angular';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'strangle-game';
+  constructor(private apollo: Apollo) {
+    this.apollo.query({
+      query: gql`
+        query {
+          users {
+            id
+            name
+            lastname
+            email
+            registerDate
+          }
+        }
+      `,
+    }).subscribe((result) => console.log('Usuarios registrados', (result.data as any).users));
+  }
 }
